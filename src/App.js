c099bede8e5b088a9efb614726c00a38cbef415f
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-11-09 13:10:33 +0530

    add base UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        textAlign: 'center',
+        marginTop: '30px',
+      }}>
+      <h1>Notes App Pro version 00.1vr </h1>
+      <p>Create and manage your notes easily</p>
     </div>
   );
 }
