1b6fe121a2920bcb279578f3c52780fa5df840e1
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-11-10 01:56:27 +0530

    WIP for Front-end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
+import { useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState(() => {
+    const saved = localStorage.getItem("notes");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  const [text, setText] = useState("");
+
+  function addNotes() {
+    if (!text.trim()) return;
+
+    const newNote = {
+      id: Date.now(),
+      text,
+      createdAt: new Date().toLocaleString(),
+    };
+
+    const update = [...notes, newNote];
+    setNotes(updated);
+    localStorage.setItem("notes", JSON.stringify(updated));
+    setText("");
+  }
+
   return (
     <div
       style={{
-        textAlign: 'center',
-        marginTop: '30px',
-      }}>
-      <h1>Notes App Pro version 00.1vr </h1>
-      <p>Create and manage your notes easily</p>
+        textAlign: "center",
+        marginTop: "30px",
+      }}
+    >
+      <h1>Notes App Pro ver </h1>
+
+      <textarea
+        rows="4"
+        cols="40"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Write Something..."
+      ></textarea>
+      <br />
     </div>
   );
 }
